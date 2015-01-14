38c70393283d8791b0f631fccc09865eddfc88ab
normalhy <vipnormalhy@gmail.com>
2015-01-14 23:00:04 +0800

    Add safety theme check

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -3,6 +3,23 @@
 (color-theme-initialize)
 (color-theme-midnight)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (warm-night)))
+ '(custom-safe-themes
+   (quote
+    ("6209442746f8ec6c24c4e4e8a8646b6324594308568f8582907d0f8f0260c3ae" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (require-package 'warm-night-theme)
 (load-theme 'warm-night)
+
 (provide 'init-color-theme)
