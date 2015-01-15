9f14efe32938bb2d49b8958cb9d8e8a057db2d06
normalhy <vipnormalhy@gmail.com>
2015-01-15 23:02:34 +0800

    Add some auto complete settings

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -10,7 +10,21 @@
 (setq-default ac-auto-start nil)
 (setq-default ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
 
-(define-key ac-mode-map  [(control return)] 'auto-complete)
+(define-key ac-mode-map [(control return)] 'auto-complete)
+
+;; ;;----------------------------------------------------------------------------
+;; ;; Use Emacs' built-in TAB completion hooks to trigger AC (Emacs >= 23.2)
+;; ;;----------------------------------------------------------------------------
+;; (setq tab-always-indent 'complete)  ;; use 't when auto-complete is disabled
+;; (add-to-list 'completion-styles 'initials t)
+;; ;; Stop completion-at-point from popping up completion buffers so eagerly
+;; (setq completion-cycle-threshold 5)
+
+;; ;; TODO: find solution for php, haskell and other modes where TAB always does something
+;; ;;----------------------------------------------------------------------------
+
+;; (setq c-tab-always-indent nil
+;;       c-insert-tab-function 'indent-for-tab-command)
 
 (set-default 'ac-sources
              '(ac-source-imenu
