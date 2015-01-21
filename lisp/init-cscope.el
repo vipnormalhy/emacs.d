1877537bf5de6da8db59730eac759289f9d4d3af
normalhy <vipnormalhy@gmail.com>
2015-01-21 18:11:53 +0800

    modify cscope update database

--- lisp/init-cscope.el
+++ lisp/init-cscope.el
@@ -3,8 +3,8 @@
 ;; use it while open c-mode or c++ mode
 (defun my-cscope-hook ()
   (require 'xcscope)
-  (cscope-setup)
-  (setq cscope-do-not-update-database t))
+  (cscope-setup))
+;;  (setq cscope-do-not-update-database t))
 
 (add-hook 'c-mode-common-hook 'my-cscope-hook)
 (add-hook 'c++-mode-common-hook 'my-cscope-hook)
