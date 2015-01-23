21d10c9b41bef712cb5a7b27f9bb8bebc32431ed
normalhy <vipnormalhy@gmail.com>
2015-01-23 13:32:51 +0800

    mod highlight symbol face color and open highlight-symbol-mode

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -4,4 +4,14 @@
 ;; (global-set-key (kbd "C-") 'highlight-symbol-next) ;;search the word where the cursor at
 ;; (global-set-key (kbd "C-#") 'highlight-symbol-prev) ;;search the word prev where the cursor at
 
+(defface highlight-symbol-face
+	 '((((class color) (background dark))
+	    (:background "#5ff"))
+	   (((class color) (background light))
+	    (:background "#5ff")))
+	 "Face used by `highlight-symbol-mode'."
+	 :group 'highlight-symbol)
+
+(highlight-symbol-mode t)
+
 (provide 'init-search)
