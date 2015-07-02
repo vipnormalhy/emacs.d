3862021dbbde2d167fce706a27e0b696bd8a2875
normalhy <vipnormalhy@gmail.com>
2015-07-02 12:08:30 +0800

    add ecb supports

--- lisp/init-cedet.el
+++ lisp/init-cedet.el
@@ -71,5 +71,11 @@
 ;; ecb settings
 (require-package 'ecb)
 (require 'ecb)
+(setq ecb-auto-activate t)
+
+(global-set-key [M-left] 'windmove-left)
+(global-set-key [M-right] 'windmove-right)
+(global-set-key [M-up] 'windmove-up)
+(global-set-key [M-down] 'windmove-down)
 
 (provide 'init-cedet)
