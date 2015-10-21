445643fcbf7ffa0d20586422b744206d2db5ef23
vipnormalhy <vipnormalhy@gmail.com>
2015-10-22 06:10:52 +0800

    add indent and python features

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,18 +1,26 @@
-(defun my-init-python-hook()
-	(require-package 'hl-indent)
-	(setq hl-indent-mode t)
-	(require-package 'flymake-python-pyflakes)
-	(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-	(require-package 'elpy)
-	(require-package 'jedi)
-	(require-package 'pycomplete)
-	(require-package 'python-mode)
-	(setq indent-tabs-mode t))
+(require-package 'smart-tab)
+(require-package 'python-mode)
+(require-package 'highlight-indentation)
 
-(add-hook 'python-mode-hook 'my-init-python-hook)
-(add-hook 'python-mode-hook
- (lambda ()
+(require 'python-mode)
+
+(setq auto-mode-alist
+       (append
+	        '(("\\.py\\'" . python-mode))
+		        ))  
+
+(add-hook 'Py-mode-hook 'my_python_setup)
+(add-hook 'python-mode-hook 'my_python_setup)
+;;(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(defun my_python_setup()
+  (setq-default indent-tabs-mode t)
   (setq indent-tabs-mode t)
-  (setq python-indent 4)))
+  (setq python-indent-offset 4)
+  (setq tab-width 4)
+  (require 'highlight-indentation)
+  (highlight-indentation-mode t)
+  (require 'smart-tab)
+  (global-smart-tab-mode t)) 
 
 (provide 'init-python)
