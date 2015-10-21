8989f286518ff8e7163061e11bfe1cb422080864
normalhy <normalhy@debian8.normalhy>
2015-10-21 11:10:00 +0800

    modify python init el

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -6,8 +6,13 @@
 	(require-package 'elpy)
 	(require-package 'jedi)
 	(require-package 'pycomplete)
+	(require-package 'python-mode)
 	(setq indent-tabs-mode t))
 
 (add-hook 'python-mode-hook 'my-init-python-hook)
+(add-hook 'python-mode-hook
+ (lambda ()
+  (setq indent-tabs-mode t)
+  (setq python-indent 4)))
 
 (provide 'init-python)
