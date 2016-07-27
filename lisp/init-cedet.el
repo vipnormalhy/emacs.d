f0d7cd1eb41ba56cac52be75ec4a33fd866e757d
vipnormalhy <vipnormalhy@gmail.com>
2016-07-28 01:32:46 +0800

    mod tab for c

--- lisp/init-cedet.el
+++ lisp/init-cedet.el
@@ -109,5 +109,6 @@
 (global-set-key (kbd "C-c o f") 'windmove-right)
 (global-set-key (kbd "C-c o p")    'windmove-up)
 (global-set-key (kbd "C-c o n")  'windmove-down)
+(setq default-tab-width 4)
 
 (provide 'init-cedet)
